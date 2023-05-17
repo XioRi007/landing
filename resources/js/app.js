361f54e0b8f7cdaf308be48b8be2130be028bd18
XioRi007 <mariyaivanova@ukr.net>
2023-05-17 16:13:37 +0300

    style refactoring

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,56 +1,56 @@
 import '../css/app.scss'
 
-const carousel = document.querySelector('.drag-n-drop_carousel')
+const carousel = document.querySelector('.carousel')
 const slides = carousel.querySelectorAll('.carousel_content')
 const prevBtn = document.querySelector('#left')
 const nextBtn = document.querySelector('#right')
 let currentPosition = 0
 prevBtn.addEventListener('click', () => {
-    if (currentPosition > 0) {
-        currentPosition -= slides[0].offsetWidth // ширина слайда
-    } else {
-        currentPosition = (slides.length - 1) * slides[0].offsetWidth
-    }
-    carousel.style.transform = `translateX(-${currentPosition}px)`
+  if (currentPosition > 0) {
+    currentPosition -= slides[0].offsetWidth // ширина слайда
+  } else {
+    currentPosition = (slides.length - 1) * slides[0].offsetWidth
+  }
+  carousel.style.transform = `translateX(-${currentPosition}px)`
 })
 
 nextBtn.addEventListener('click', () => {
-    if (currentPosition < (slides.length - 1) * slides[0].offsetWidth) {
-        currentPosition += slides[0].offsetWidth
-    } else {
-        currentPosition = 0
-    }
-    carousel.style.transform = `translateX(-${currentPosition}px)`
+  if (currentPosition < (slides.length - 1) * slides[0].offsetWidth) {
+    currentPosition += slides[0].offsetWidth
+  } else {
+    currentPosition = 0
+  }
+  carousel.style.transform = `translateX(-${currentPosition}px)`
 })
 
 const links = document.querySelectorAll('a[href^="#"]')
 
 links.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault()
-        const id = link.getAttribute('href').slice(1)
-        const targetElement = document.getElementById(id)
-        if (targetElement) {
-            const offsetTop = targetElement.getBoundingClientRect().top + window.scrollY
-            window.scrollTo({
-                top: offsetTop,
-                behavior: 'smooth'
-            })
-        }
-    })
+  link.addEventListener('click', (event) => {
+    event.preventDefault()
+    const id = link.getAttribute('href').slice(1)
+    const targetElement = document.getElementById(id)
+    if (targetElement) {
+      const offsetTop = targetElement.getBoundingClientRect().top + window.scrollY
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth'
+      })
+    }
+  })
 })
 
-const burgerBnt = document.querySelector('.burger-menu-btn')
+const burgerBnt = document.querySelector('.burger_menu_btn')
 const nav = document.querySelector('.header_nav')
-const icon = document.querySelector('.burger-menu-icon')
+const icon = document.querySelector('.burger_menu_icon')
 burgerBnt.addEventListener('click', () => {
-    nav.classList.toggle('header_nav-active')
-    icon.classList.toggle('icon-active')
+  nav.classList.toggle('header_nav_active')
+  icon.classList.toggle('icon_active')
 })
 
 nav.addEventListener('click', (event) => {
-    if (nav.classList.contains('header_nav-active') && event.target.tagName === 'A') {
-        nav.classList.remove('header_nav-active')
-        icon.classList.remove('icon-active')
-    }
+  if (nav.classList.contains('header_nav_active') && event.target.tagName === 'A') {
+    nav.classList.remove('header_nav_active')
+    icon.classList.remove('icon_active')
+  }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -5,65 +5,53 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry: ['./resources/js/app.js', './resources/css/app.scss'],
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'app.js',
-        assetModuleFilename: './img/[name][ext]'
-    },
-    module: {
-        rules: [
-            // {
-            //     test: /\.css$/i,
-            //     use: [MiniCssExtractPlugin.loader, {
-            //         loader: 'css-loader',
-            //         options: {
-            //             url: false
-            //         }
-            //     }],
-            // },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader'
-                ]
-            }
+  entry: ['./resources/js/app.js', './resources/css/app.scss'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.js',
+    assetModuleFilename: './img/[name][ext]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
-    },
-    devServer:{
-        static: {
-            directory: path.join(__dirname, 'public')
-        },
-        compress: true,
-        port: 9000
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './resources/index.html',
-            options: {
-                root: path.resolve(__dirname, 'public')
-            }
-        }),
-        new WebpackManifestPlugin({
-            fileName: 'manifest.json',
-            publicPath: '/public/'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'styles.css'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'resources/img', to: 'img' }
-            ]
-        })
+      }
     ]
+  },
+  devServer:{
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './resources/index.html',
+      options: {
+        root: path.resolve(__dirname, 'public')
+      }
+    }),
+    new WebpackManifestPlugin({
+      fileName: 'manifest.json',
+      publicPath: '/public/'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'resources/img', to: 'img' }
+      ]
+    })
+  ]
 }
